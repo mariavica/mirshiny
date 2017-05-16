94b682a2fb41589479f7cd5f2cd098844c0337c5
mariavica <maria.vila.cs@gmail.com>
2017-05-16 10:30:37 +0200

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,25 +1,5 @@
-shinyUI(bootstrapPage(
+fluidPage(
+  titlePanel("MiRTranslator")
+)
 
-  selectInput(inputId = "n_breaks",
-              label = "Number of bins in histogram (approximatedddddd):",
-              choices = c(10, 20, 35, 50),
-              selected = 20),
-
-  checkboxInput(inputId = "individual_obs",
-                label = strong("Show individual observations"),
-                value = FALSE),
-
-  checkboxInput(inputId = "density",
-                label = strong("Show density estimate"),
-                value = FALSE),
-
-  plotOutput(outputId = "main_plot", height = "300px"),
-
-  # Display this only if the density is shown
-  conditionalPanel(condition = "input.density == true",
-    sliderInput(inputId = "bw_adjust",
-                label = "Bandwidth adjustment:",
-                min = 0.2, max = 2, value = 1, step = 0.2)
-  )
-
-))
+textInput(inputId = "mirname")
