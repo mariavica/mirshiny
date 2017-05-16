2bc748b69e5d6a7326ee0fb660049340c24a2077
mariavica <maria.vila.cs@gmail.com>
2017-05-16 12:08:20 +0200

    trying to read table

--- server.r
+++ server.r
@@ -1,8 +1,11 @@
 shinyServer(function(input, output) {
-
+   mirtable <- read.table("data/miRBase_conversion.csv",header=TRUE,sep="\t")
+   
    text1 <- eventReactive(input$goButton, {
       input$mirname
    })
+   
+   text1<-mirtable[1,1]
    output$text <- renderText({
       text1()
    })
