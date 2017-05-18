7120d50b0967da494900795ca7ad67479f102612
mariavica <maria.vila.cs@gmail.com>
2017-05-18 14:48:35 +0200

    Almost working version

--- server.r
+++ server.r
@@ -4,55 +4,154 @@ shinyServer(function(input, output) {
    
    mymirnas <- eventReactive(input$goButton, {
       input$mirname
+     
    })
    
-   output$text1 <- renderText({
-      paste("Your miRNA is:",input$mirname,"\n")
-   })      
-   output$text2 <- renderText({
-      paste("The header of mirtable is:",versions.mirnas[1,1])
+   
+   mirto <- eventReactive(input$goButton, {
+     input$mirto
+      })
+   
+   mirfrom <- eventReactive(input$goButton, {
+     input$mirfrom
    })
+   
    
    
     output$percent <- renderPlot({
-	    
-	versions.mirnas<-versions.mirnas[,-1]
-
-	perc <- function (x,target) {
-		return(length(which((target %in% x) == TRUE)) / length(target) *100)
-	}
-
-	    mymirnas<-unlist(strsplit(as.character(input$mirname)," "))
-	    print(mymirnas)
-	    
-	a<-apply(versions.mirnas,2,perc,mymirnas)
-
-	dat<-data.frame(x=names(a),y=a)
-
-	dat$x<-factor(gsub("miRBase_","",dat$x))
-	dat$x<-relevel(dat$x,"9.2")
-	dat$x<-relevel(dat$x,"9.1")
-	dat$x<-relevel(dat$x,"9.0")
-	dat$x<-relevel(dat$x,"8.2")
-	dat$x<-relevel(dat$x,"8.1")
-	dat$x<-relevel(dat$x,"8.0")
-	dat$x<-relevel(dat$x,"7.1")
-	dat$x<-relevel(dat$x,"7.0")
-	dat$x<-relevel(dat$x,"6.0")
-   print(dat)
-	    
-   #		qplot(x=dat$x, y=dat$y, geom="bar", stat="identity", fill=dat$x) +
-  #          guides(fill=FALSE) + xlab("miRBase version") + ylab("Coincidence (%)")
-   		
-   		
+      perc <- function (x,target) {
+        return(length(which((target %in% x) == TRUE)) / length(target) *100)
+      }
+      
+      versions.mirnas<-versions.mirnas[,-1]
+      
+      mymirnas<-unlist(strsplit(as.character(input$mirname),c("\\,|\\ |\\\n")))
+      print(mymirnas)
+      
+      a<-apply(versions.mirnas,2,perc,mymirnas)
+      
+      dat<-data.frame(x=names(a),y=a)
+      
+      dat$x<-factor(gsub("miRBase_","",dat$x))
+      dat$x<-relevel(dat$x,"9.2")
+      dat$x<-relevel(dat$x,"9.1")
+      dat$x<-relevel(dat$x,"9.0")
+      dat$x<-relevel(dat$x,"8.2")
+      dat$x<-relevel(dat$x,"8.1")
+      dat$x<-relevel(dat$x,"8.0")
+      dat$x<-relevel(dat$x,"7.1")
+      dat$x<-relevel(dat$x,"7.0")
+      dat$x<-relevel(dat$x,"6.0")
+      print(dat)
+      
+      maxs<-which(dat$y==max(dat$y))
+      selectedversion<-dat[maxs[length(maxs)] ,"x"]	
+      
+	   
    		qplot(x=dat$x, y=dat$y, fill=dat$x) + geom_bar(stat="identity") +
    		  guides(fill=FALSE) + xlab("miRBase version") + ylab("Coincidence (%)")
-   		
-   		
-   		
-   		
+
    })
    
-   
+    output$text1 <- renderText({
+      
+      perc <- function (x,target) {
+        return(length(which((target %in% x) == TRUE)) / length(target) *100)
+      }
+      
+      versions.mirnas<-versions.mirnas[,-1]
+      
+      mymirnas<-unlist(strsplit(as.character(input$mirname),c("\\,|\\ |\\\n")))
+      print(mymirnas)
+      
+      a<-apply(versions.mirnas,2,perc,mymirnas)
+      
+      dat<-data.frame(x=names(a),y=a)
+      
+      dat$x<-factor(gsub("miRBase_","",dat$x))
+      dat$x<-relevel(dat$x,"9.2")
+      dat$x<-relevel(dat$x,"9.1")
+      dat$x<-relevel(dat$x,"9.0")
+      dat$x<-relevel(dat$x,"8.2")
+      dat$x<-relevel(dat$x,"8.1")
+      dat$x<-relevel(dat$x,"8.0")
+      dat$x<-relevel(dat$x,"7.1")
+      dat$x<-relevel(dat$x,"7.0")
+      dat$x<-relevel(dat$x,"6.0")
+      #print(dat)
+      
+      maxs<-which(dat$y==max(dat$y))
+      selectedversion<-dat[maxs[length(maxs)] ,"x"]	
+      
+      
+      paste("Your miRNA version is:",selectedversion,"\n")
+    })  
+    
+      
+    output$translated <- renderTable({
+      
+      if (as.character(input$mirfrom) == "I don't know") {
+      
+      
+      perc <- function (x,target) {
+        return(length(which((target %in% x) == TRUE)) / length(target) *100)
+      }
+      
+      versions.mirnas<-versions.mirnas[,-1]
+      
+      mymirnas<-unlist(strsplit(as.character(input$mirname),c("\\,|\\ |\\\n")))
+      print(mymirnas)
+      
+      a<-apply(versions.mirnas,2,perc,mymirnas)
+      
+      dat<-data.frame(x=names(a),y=a)
+      
+      dat$x<-factor(gsub("miRBase_","",dat$x))
+      dat$x<-relevel(dat$x,"9.2")
+      dat$x<-relevel(dat$x,"9.1")
+      dat$x<-relevel(dat$x,"9.0")
+      dat$x<-relevel(dat$x,"8.2")
+      dat$x<-relevel(dat$x,"8.1")
+      dat$x<-relevel(dat$x,"8.0")
+      dat$x<-relevel(dat$x,"7.1")
+      dat$x<-relevel(dat$x,"7.0")
+      dat$x<-relevel(dat$x,"6.0")
+      print(dat)
+      
+      maxs<-which(dat$y==max(dat$y))
+      selectedversion<-dat[maxs[length(maxs)] ,"x"]	
+      } else {
+        selectedversion<-as.character(input$mirfrom)
+      }
+      
+      print(paste("miRBase_",selectedversion,sep=""))
+      
+        
+      mymirnas<-as.character(mymirnas)
+      
+      mytrans<-data.frame(mymirnas)
+      
+      for (i in 1:nrow(mytrans)) {
+        if (mymirnas[i] %in% versions.mirnas[,paste("miRBase_",selectedversion,sep="")] ) {
+          mytrans[i,2]<-as.character(versions.mirnas[ which(  (versions.mirnas[,paste("miRBase_",selectedversion,sep="")] %in%  mymirnas[i])  ), c(paste("miRBase_",as.character(input$mirto),sep="")) ])
+          
+        } else {
+          
+          mytrans[i,2]<-"Not in input"
+          
+        }
+      }
+        
+      colnames(mytrans)<-c("Original_names",c(paste("miRBase_",as.character(input$mirto),sep="")))
+      
   
+  
+      
+      
+      print(mytrans)
+    })
+    
+    
+  
+    
 })
--- ui.r
+++ ui.r
@@ -3,8 +3,8 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Write here your miRNA name"),
-      textInput(inputId = "mirname", label=NULL),
+      helpText("Paste here your miRNAs"),
+      textAreaInput(inputId = "mirname", label=NULL,cols=20,rows=5),
       helpText("or update your file"),
       fileInput(inputId = "mirfile", label=NULL, multiple = FALSE, accept = NULL, width = NULL, 
                 buttonLabel = "Browse...", placeholder = "No file selected"),
@@ -16,16 +16,25 @@ shinyUI(fluidPage(
       
       helpText("Your desired miRBase version"),
       selectInput(inputId = "mirto", label=NULL, choices=c("6.0", "7.0", "7.1", "8.0", "8.1", "8.2", "9.0", "9.1", "9.2", "10.0", "10.1", "11.0", "12.0", "13.0", "14", "15", "16", "17", "18", "19", "20", "21" ),
-                  selected = "I don't know", multiple = FALSE, selectize = TRUE, width = NULL, size = NULL),
+                  selected = "21", multiple = FALSE, selectize = TRUE, width = NULL, size = NULL),
       
       actionButton("goButton", "Go")
     ),    
     
     mainPanel(
-      textOutput("text1", container=span),
-      textOutput("text2", container=span),
-      plotOutput("percent")
+      #textOutput("text2", container=span),
+      helpText("Coincidences across miRNA versions"),
+      plotOutput("percent"),
 
+      textOutput("text1", container=span),
+      
+      helpText("Your translated miRNAs are:"),
+      
+      
+      tableOutput("translated")
+      
+      
+      
     )
   )  
 ))
