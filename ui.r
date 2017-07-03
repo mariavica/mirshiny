1176037f222be1812d68f645f08fa2829bc187df
mariavica <maria.vila.cs@gmail.com>
2017-07-03 12:12:21 +0200

    Sort better output page

--- ui.r
+++ ui.r
@@ -3,18 +3,19 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Paste here your miRNAs to start:"),
+      
+      helpText("Upload your file (\"*txt\" or \"*csv\" plain file):"),
+      
+      fileInput(inputId="csvfile", label=NULL , multiple = FALSE, accept = c(
+        "text/csv", "text/comma-separated-values,text/plain",".csv",".txt"), width = NULL,
+        buttonLabel = "Browse...", placeholder = "No file selected"),
+      
+      helpText("or paste here your miRNAs to start:"),
       textAreaInput(inputId = "mirname", label=NULL,cols=20,rows=17),
   #    helpText("or update your file"),
   #    fileInput(inputId = "mirfile", label=NULL, multiple = FALSE, accept = NULL, width = NULL, 
   #              buttonLabel = "Browse...", placeholder = "No file selected"),
-      
-      helpText("Or upload file"),
-   
-      fileInput(inputId="csvfile", label=NULL , multiple = FALSE, accept = c(
-        "text/csv", "text/comma-separated-values,text/plain",".csv",".txt"), width = NULL,
-            buttonLabel = "Browse...", placeholder = "No file selected"),
-  
+
       helpText("Your miRBase version"),
             
       selectInput(inputId = "mirfrom", label=NULL, choices=c("I don't know", "6.0", "7.0", "7.1", "8.0", "8.1", "8.2", "9.0", "9.1", "9.2", "10.0", "10.1", "11.0", "12.0", "13.0", "14", "15", "16", "17", "18", "19", "20", "21" ),
@@ -28,9 +29,9 @@ shinyUI(fluidPage(
   
   
   
-        checkboxInput(inputId="forceTranslation",label="Force translation (miRNAs from other versions will be automatically translated to final version)", value=FALSE),
+        checkboxInput(inputId="forceTranslation",label="Force translation (miRNAs from other versions will be automatically translated to desired version)", value=FALSE),
 
-  checkboxInput(inputId="capitalise",label="Correct capital letters", value=FALSE),
+  checkboxInput(inputId="capitalise",label="Correct capital letters (valid only for \"miR\" and \"let\")", value=FALSE),
   
     
   helpText("MiRNA species"),
